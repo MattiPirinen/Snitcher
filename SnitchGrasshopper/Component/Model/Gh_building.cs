8fa9c12360ac5dbc67b38b1ea2291a39bedc2418
PirisenMatti <matti.pirinen@ramboll.fi>
2023-04-15 16:41:45 +0300

    wip

--- SnitchGrasshopper/Component/Model/Gh_building.cs
+++ SnitchGrasshopper/Component/Model/Gh_building.cs
@@ -1,4 +1,8 @@
-﻿using Grasshopper.Kernel;
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
 using SnitchCommon;
 using System;
 using System.Collections.Generic;
@@ -34,6 +38,8 @@ namespace SnitchGrasshopper.Component.Model
         }
 
         public List<BuildingMember_base> InputObjects { get; set; }
+
+        public GH_Structure<GH_Curve> Voronois { get; set; }
         public Building Building { get; set; }
 
         //------------------------ METHODS ---------------------------
@@ -42,6 +48,7 @@ namespace SnitchGrasshopper.Component.Model
         {
             //#0
             pManager.AddGenericParameter("List of building objects", "Objects", "List of building objects", GH_ParamAccess.list);
+            pManager.AddCurveParameter("Voronois", "Voronois", "Voronois", GH_ParamAccess.tree);
         }
 
         protected override void RegisterOutputParams(GH_OutputParamManager pManager)
@@ -53,24 +60,41 @@ namespace SnitchGrasshopper.Component.Model
         private bool CollectInputData_buildingObjects(IGH_DataAccess DA)
         {
             List<BuildingMember_base> list_temp = new List<BuildingMember_base>();
-
+            GH_Structure<GH_Curve> curves = new GH_Structure<GH_Curve>();
             if (!DA.GetDataList(0, list_temp))
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "Failed to receive input for list of building objects");
                 return false;
             }
+            if (!DA.GetDataTree(1, out curves))
+                return false;
+            
+            
+
 
             InputObjects = list_temp;
-
+            Voronois = curves;
             return true;
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
             if (CollectInputData_buildingObjects(DA) == false) { return; }
-
-
             Building = new Building(InputObjects);
+
+            List<double> floorCoordsZ = new List<double>();
+            
+            foreach (var item in Voronois.Branches)
+            {
+                
+                item[0].Value.TryGetPolyline(out var polyline);
+                floorCoordsZ.Add(polyline[0].Z);
+            }
+            foreach (var column in Building.Columns.Values)
+            {
+            }
+
+
 
             AssignOutputVariables(DA);
         }
