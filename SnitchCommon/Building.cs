f36ae11c4bcc3adcd2dd02d4849eb5e0fc6459f7
Pooya Saremi <pooya.saremi@ramboll.fi>
2023-04-15 18:40:25 +0300

    BackEnd: wip on database

--- SnitchCommon/Building.cs
+++ SnitchCommon/Building.cs
@@ -12,16 +12,17 @@ namespace SnitchCommon
         public Building()
         {
             AssignProperties();
+            this.Calculation = new Calculation(this);
         }
-
         public Building(List<BuildingMember_base> gh_inputObjs)
         {
             AssignProperties();
-            DetectAndPopulateObjects(gh_inputObjs);
-            ProcessFloorInformation();
+            this.Calculation = new Calculation(this, gh_inputObjs);
         }
 
         //----------------------- PROPERTIES -------------------------
+        public Calculation Calculation { get; set; }
+
         public int FloorQty_total { get; set; }
         public double DistributedLoad_live { get; set; }
 
@@ -31,7 +32,7 @@ namespace SnitchCommon
         public CO2Emission CO2_slabs { get; set; }
         public CO2Emission CO2_walls { get; set; }
 
-        public List<Dictionary<Guid, BuildingMember_base>> BuildingObjectsList { get; private set; }
+        
         public Dictionary<Guid, BuildingMember_base> Beams { get; private set; }
         public Dictionary<Guid, BuildingMember_base> Columns{ get; private set; }
         public Dictionary<Guid, BuildingMember_base> Slabs { get; private set; }
@@ -48,137 +49,6 @@ namespace SnitchCommon
             this.Walls= new Dictionary<Guid, BuildingMember_base>();
 
             this.DistributedLoad_live = 2000; // N/m2
-        }
-
-        private void InitiateObjectList()
-        {
-            this.BuildingObjectsList = new List<Dictionary<Guid, BuildingMember_base>>()
-            {
-                this.Beams,
-                this.Columns,
-                this.Walls,
-                this.Slabs
-            };
-        }
-
-        public void Calculate_CO2_and_score(AverageCo2Values averageCo2Values)
-        {
-            
-            InitiateObjectList();
-
-            foreach (Dictionary<Guid, BuildingMember_base> dict in this.BuildingObjectsList)
-            {
-                foreach (KeyValuePair<Guid, BuildingMember_base> kvp in dict)
-                {
-                    kvp.Value.CalculateProperties(averageCo2Values);
-
-                    CollectCO2(kvp.Value);
-                }
-            }
-        }
-
-        private void CollectCO2(BuildingMember_base obj)
-        {
-            this.CO2_total.CollectCO2(obj.CO2);
-
-            if(obj is Beam) 
-            { 
-                this.CO2_beams.CollectCO2(obj.CO2); 
-            }
-            else if(obj is Column)
-            {
-                this.CO2_columns.CollectCO2(obj.CO2);
-            }
-            else if(obj is Wall)
-            {
-                this.CO2_walls.CollectCO2(obj.CO2);
-            }
-            else if(obj is Slab)
-            {
-                this.CO2_slabs.CollectCO2(obj.CO2);
-            }
-        }
-
-        private void DetectAndPopulateObjects(List<BuildingMember_base> gh_inputObjs)
-        {
-            foreach (BuildingMember_base item in gh_inputObjs)
-            {
-                DetectAndPopulateObject(item);
-            }
-        }
-
-        private void DetectAndPopulateObject(BuildingMember_base item)
-        {
-            if(item is Beam beam) 
-            { 
-                this.Beams.Add(beam.Guid, beam); 
-            }
-            else if(item is Column column)
-            {
-                this.Columns.Add(column.Guid, column);
-            }
-            else if(item is Wall wall)
-            {
-                this.Walls.Add(wall.Guid, wall);
-            }
-            else if(item is Slab slab)
-            {
-                this.Slabs.Add(slab.Guid, slab);
-            }
-            else
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        
-        private void ProcessFloorInformation()
-        {
-            List<KeyValuePair<double, List<Column>>> list = CollectFloorColumns();
-
-            this.FloorQty_total = list.Count;
-
-            SetFloorNumberAndLoadToColumns(list);
-        }
-
-        private List<KeyValuePair<double, List<Column>>> CollectFloorColumns()
-        {
-            Dictionary<double, List<Column>> minZCoordinates = new Dictionary<double, List<Column>>();
-
-            foreach (Column column in this.Columns.Values)
-            {
-                BoundingBox bb = BoundingBox.Empty;
-
-                bb = column.CenterLine.ToNurbsCurve().GetBoundingBox(true);
-
-                double minZ = Math.Round(bb.Min.Z, 1);
-
-                if (minZCoordinates.ContainsKey(minZ))
-                    minZCoordinates[minZ].Add(column);
-                else
-                    minZCoordinates.Add(minZ, new List<Column>() { column });
-            }
-
-            List<KeyValuePair<double, List<Column>>> list = minZCoordinates.ToList();
-
-            return list.OrderBy(l => l.Key).ToList();
-        }
-
-        private void SetFloorNumberAndLoadToColumns(List<KeyValuePair<double, List<Column>>> list)
-        {
-            for (int i = 1; i < list.Count; i++)
-            {
-                KeyValuePair<double, List<Column>> kvp = list[i];
-
-                foreach (Column column in kvp.Value)
-                {
-                    column.FloorNo = i;
-
-                    column.CalculateLoad(this.FloorQty_total, this.DistributedLoad_live);
-                }
-
-
-            }
         }
 
     }
---
+++ SnitchCommon/Calculation.cs
@@ -0,0 +1,212 @@
+﻿using Newtonsoft.Json;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SnitchCommon
+{
+    public class Calculation
+    {
+        //---------------------- CONSTRUCTORS ------------------------
+        public Calculation(Building building)
+        {
+            //Building = building;
+
+            //DetectAndPopulateObjects(gh_inputObjs);
+            //ProcessFloorInformation();
+
+            InstantiateDataBAse();
+        }
+        public Calculation(Building building, List<BuildingMember_base> gh_inputObjs)
+        {
+            Building = building;
+
+            DetectAndPopulateObjects(gh_inputObjs);
+            ProcessFloorInformation();
+
+            InstantiateDataBAse();
+        }
+
+        //----------------------- PROPERTIES -------------------------
+
+        public Building DataBase { get; set; }
+        public Building Building { get; set; }
+        public List<Dictionary<Guid, BuildingMember_base>> BuildingObjectsList { get; private set; }
+
+
+        //------------------------ METHODS ---------------------------
+
+        private void InitiateObjectList()
+        {
+            this.BuildingObjectsList = new List<Dictionary<Guid, BuildingMember_base>>()
+            {
+                this.Building.Beams,
+                this.Building.Columns,
+                this.Building.Walls,
+                this.Building.Slabs
+            };
+        }
+
+        public void Calculate_CO2_and_score(AverageCo2Values averageCo2Values)
+        {
+
+            InitiateObjectList();
+
+            foreach (Dictionary<Guid, BuildingMember_base> dict in this.BuildingObjectsList)
+            {
+                foreach (KeyValuePair<Guid, BuildingMember_base> kvp in dict)
+                {
+                    kvp.Value.CalculateProperties(averageCo2Values);
+
+                    CollectCO2(kvp.Value);
+                }
+            }
+        }
+
+        private void CollectCO2(BuildingMember_base obj)
+        {
+            this.Building.CO2_total.CollectCO2(obj.CO2);
+
+            if (obj is Beam)
+            {
+                this.Building.CO2_beams.CollectCO2(obj.CO2);
+            }
+            else if (obj is Column)
+            {
+                this.Building.CO2_columns.CollectCO2(obj.CO2);
+            }
+            else if (obj is Wall)
+            {
+                this.Building.CO2_walls.CollectCO2(obj.CO2);
+            }
+            else if (obj is Slab)
+            {
+                this.Building.CO2_slabs.CollectCO2(obj.CO2);
+            }
+        }
+
+        private void DetectAndPopulateObjects(List<BuildingMember_base> gh_inputObjs)
+        {
+            foreach (BuildingMember_base item in gh_inputObjs)
+            {
+                DetectAndPopulateObject(item);
+            }
+        }
+
+        private void DetectAndPopulateObject(BuildingMember_base item)
+        {
+            if (item is Beam beam)
+            {
+                this.Building.Beams.Add(beam.Guid, beam);
+            }
+            else if (item is Column column)
+            {
+                this.Building.Columns.Add(column.Guid, column);
+            }
+            else if (item is Wall wall)
+            {
+                this.Building.Walls.Add(wall.Guid, wall);
+            }
+            else if (item is Slab slab)
+            {
+                this.Building.Slabs.Add(slab.Guid, slab);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        private void ProcessFloorInformation()
+        {
+            List<KeyValuePair<double, List<Column>>> list = CollectFloorColumns();
+
+            this.Building.FloorQty_total = list.Count;
+
+            SetFloorNumberAndLoadToColumns(list);
+        }
+
+        private List<KeyValuePair<double, List<Column>>> CollectFloorColumns()
+        {
+            Dictionary<double, List<Column>> minZCoordinates = new Dictionary<double, List<Column>>();
+
+            foreach (Column column in this.Building.Columns.Values)
+            {
+                BoundingBox bb = BoundingBox.Empty;
+
+                bb = column.CenterLine.ToNurbsCurve().GetBoundingBox(true);
+
+                double minZ = Math.Round(bb.Min.Z, 1);
+
+                if (minZCoordinates.ContainsKey(minZ))
+                    minZCoordinates[minZ].Add(column);
+                else
+                    minZCoordinates.Add(minZ, new List<Column>() { column });
+            }
+
+            List<KeyValuePair<double, List<Column>>> list = minZCoordinates.ToList();
+
+            return list.OrderBy(l => l.Key).ToList();
+        }
+
+        private void SetFloorNumberAndLoadToColumns(List<KeyValuePair<double, List<Column>>> list)
+        {
+            for (int i = 1; i <= list.Count; i++)
+            {
+                KeyValuePair<double, List<Column>> kvp = list[i];
+
+                foreach (Column column in kvp.Value)
+                {
+                    column.FloorNo = i;
+
+                    column.CalculateLoad(this.Building.FloorQty_total, this.Building.DistributedLoad_live);
+                }
+
+
+            }
+        }
+
+        private void InstantiateDataBAse()
+        {
+            string jsonString = ReadDataBase_generic("Database");
+            List < BuildingMember_base > list = JsonConvert.DeserializeObject<List<BuildingMember_base>>(jsonString);
+        }
+
+
+        private string ReadDataBase_generic(string dbName)
+        {
+            string text = "";
+
+            string path = GetResourcePath(dbName);
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            using (Stream stream = assembly.GetManifestResourceStream(path))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            return text;
+        }
+
+        private string GetResourcePath(string keyword)
+        {
+            string path = "";
+
+            List<string> resrourcePaths = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceNames().ToList();
+
+            path = resrourcePaths.First(rp => rp.Contains(keyword));
+
+            return path;
+        }
+
+
+    }
+}
